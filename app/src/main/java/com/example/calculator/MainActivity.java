d71bee8647d4d0c8154b388260b082eeabd577bf
miglesuminaite <159789395+miglesuminaite@users.noreply.github.com>
2024-11-10 19:27:24 +0200

    Added setNumberButtonListeners method to handle number button clicks and fixed errors

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -24,4 +24,17 @@ public class MainActivity extends AppCompatActivity {
         setNumberButtonListeners();
         setOperationButtonListeners();
     }
+
+    private void setNumberButtonListeners() {
+        int[] numberButtonIds = {R.id.btn0, R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4, R.id.btn5, R.id.btn6, R.id.btn7, R.id.btn8, R.id.btn9, R.id.btnDecimal};
+        View.OnClickListener listener = v -> {
+            Button b = (Button) v;
+            if (isNewOp) txtDisplay.setText("");
+            isNewOp = false;
+            txtDisplay.append(b.getText().toString());
+        };
+        for (int id : numberButtonIds) findViewById(id).setOnClickListener(listener);
+    }
+
+
 }
